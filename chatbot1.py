52ffe16aecabdf830b09d98e90f9fd2a0c54c3c4
Fabian <fabian.van.treek@gmx.de>
2023-01-10 10:41:09 +0100

    Add ModeratorTest

--- chatbot1.py
+++ chatbot1.py
@@ -1,23 +1,27 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec 16 17:54:58 2022
+Created on Fri Jan  6 13:50:02 2023
 
-@author: Fabian
+@author: Laura
 """
-
-from chatbot_intents_methods import get_bot_response
 
 import random
 from typing import List
-
-
 
 from chatbotsclient.chatbot import Chatbot
 from chatbotsclient.message import Message
 
 
 def random_answer(message, conversation):
-    return get_bot_response(message)
+    return random.choice(
+        [
+            "I like zucchini.",
+            "I love butter.",
+            "I prefer müsli.",
+            "Avocados are my favorite.",
+        ]
+    )
 
 
 def respond(message: Message, conversation: List[Message]):
@@ -26,4 +30,4 @@ def respond(message: Message, conversation: List[Message]):
     return answer
 
 
-chatbot = Chatbot(respond, "Hope")
+chatbot = Chatbot(respond, "Chatbot 1")
